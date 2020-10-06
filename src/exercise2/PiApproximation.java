aa20c7a1abd4f56cf793be3bb1ca0cd2120b0313
salinapencheva <feisu@abv.bg>
2020-10-06 15:13:22 +0000

    a1

---
+++ src/exercise2/PiApproximation.java
@@ -0,0 +1,11 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class PiApproximation  {
+
+  public static void main(String[] args) {
+double pi = 4 * (1 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/11 - 1.0/13 + 1.0/17 - 1.0/19 + 1.0/23);
+System.out.println ("Pi: " + pi);
+  }
+}
